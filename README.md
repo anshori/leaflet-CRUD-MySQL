90c291a680910e6a510bf0ba77a2c1960b6e1f5b
unsorry <m.anshory@gmail.com>
2020-12-11 15:26:40 +0700

    [Commit 2020-12-11]
    - First upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # leaflet-CRUD-MySQL
+
 Leaflet.JS CRUD Implementation to MySQL
+
+This repository adopts from [https://github.com/andyprasetya/leaflet-CRUD](https://github.com/andyprasetya/leaflet-CRUD)
+
+This project uses [Leaflet](https://github.com/Leaflet/Leaflet), [Leaflet.Draw](https://github.com/Leaflet/Leaflet.draw), [ESRI Terraformer Core](http://terraformer.io/core/) and [ESRI Terraformer WKT Parser](http://terraformer.io/wkt-parser/); while at the backend it uses [PHP](http://php.net/) and [MySQL](https://www.mysql.com/).
+
+Demo: [https://leafletcrudmysql.unsorry.net/](https://leafletcrudmysql.unsorry.net/)
+
+![Screenshot](./archive/screenshot.png)
+
+`unsorry@2020`
---
+++ archive/leaflet_crud.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2020 at 04:56 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `leaflet_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_linestring`
+--
+
+CREATE TABLE `data_linestring` (
+  `gid` int(11) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `geom` geometry NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_point`
+--
+
+CREATE TABLE `data_point` (
+  `gid` int(11) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `geom` geometry NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_polygon`
+--
+
+CREATE TABLE `data_polygon` (
+  `gid` int(11) NOT NULL,
+  `notes` varchar(255) NOT NULL,
+  `geom` geometry NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data_linestring`
+--
+ALTER TABLE `data_linestring`
+  ADD PRIMARY KEY (`gid`);
+
+--
+-- Indexes for table `data_point`
+--
+ALTER TABLE `data_point`
+  ADD PRIMARY KEY (`gid`);
+
+--
+-- Indexes for table `data_polygon`
+--
+ALTER TABLE `data_polygon`
+  ADD PRIMARY KEY (`gid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data_linestring`
+--
+ALTER TABLE `data_linestring`
+  MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `data_point`
+--
+ALTER TABLE `data_point`
+  MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `data_polygon`
+--
+ALTER TABLE `data_polygon`
+  MODIFY `gid` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
