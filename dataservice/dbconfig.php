7887f5e63fff63bd7645982648db9e81c985824c
unsorry <m.anshory@gmail.com>
2020-12-22 09:03:58 +0700

    [Commit 2020-12-22]
    ignore dbconfig.php

--- dataservice/dbconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $dsn = 'mysql:dbname=[__customise_it_];host=127.0.0.1';
-  $user = '[__customise_it_]';
-  $password = '[__customise_it_]';
-
-  $dbcon = new PDO($dsn, $user, $password);
-?>
